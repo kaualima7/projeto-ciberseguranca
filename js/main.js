276e02d578297c97dd2368d2c14aeee335d7b05b
kaualima7 <kaualima2007@outlook.com>
2025-06-23 21:58:56 -0300

    ajuste dark/light mode e form contato

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-document.getElementById("contato-form").addEventListener("submit",function(event) {
-      event.preventDefault();
-      alert("Página em construção");
-    })
+console.log('Script carregado');
+console.log('botaoTema:', document.getElementById('toggle-theme'));
+console.log('icone:', document.getElementById('icone-tema'));
+
+const formContato = document.getElementById("contato-form");
+if (formContato) {
+  formContato.addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Página em construção");
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const botaoTema = document.getElementById('toggle-theme');
+  const icone = document.getElementById('icone-tema');
+
+  if (botaoTema && icone) {
+    botaoTema.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+      const isDark = document.body.classList.contains('dark');
+      icone.src = isDark ? './img/lightMode.svg' : './img/darkMode.svg';
+    });
+  }
+});
+
+
