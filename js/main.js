797c3aed0cf929f9a037219032b056c38051d48e
kaualima7 <kaualima2007@outlook.com>
2025-06-23 22:55:44 -0300

    1 mudança acessibilidade

--- js/main.js
+++ js/main.js
@@ -23,4 +23,21 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+const botaoTema = document.getElementById('toggle-theme');
+const icone = document.getElementById('icone-tema');
+
+const iconeAumentar = document.getElementById('icone-aumentar');
+const iconeDiminuir = document.getElementById('icone-diminuir');
+
+if (botaoTema && icone && iconeAumentar && iconeDiminuir) {
+  botaoTema.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const isDark = document.body.classList.contains('dark');
+    icone.src = isDark ? './img/lightMode.svg' : './img/darkMode.svg';
+    iconeAumentar.src = isDark ? './img/A+prodark.svg' : './img/A+proLight.svg';
+    iconeDiminuir.src = isDark ? './img/A-prodark.svg' : './img/A-proLight.svg';
+  });
+}
+
+
 
